f6426d414865392e793d8f1390c11565e0e24951
lxb <424151385@qq.com>
2017-03-20 18:09:55 +0800

    base2

---
+++ app/src/main/java/com/huaxin/lxb/test2/PermissionActivity.java
@@ -0,0 +1,172 @@
+package com.huaxin.lxb.test2;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+    }
+
+    private PermissionsResultListener mListener;
+
+    private int mRequestCode;
+
+    /**
+     * 其他 Activity 继承 BaseActivity 调用 performRequestPermissions 方法
+     *
+     * @param desc        首次申请权限被拒绝后再次申请给用户的描述提示
+     * @param permissions 要申请的权限数组
+     * @param requestCode 申请标记值
+     * @param listener    实现的接口
+     */
+    protected void performRequestPermissions(String desc, String[] permissions, int requestCode, PermissionsResultListener listener) {
+        if (permissions == null || permissions.length == 0) {
+            return;
+        }
+        mRequestCode = requestCode;
+        mListener = listener;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkEachSelfPermission(permissions)) {// 检查是否声明了权限
+                requestEachPermissions(desc, permissions, requestCode);
+            } else {// 已经申请权限
+                if (mListener != null) {
+                    mListener.onPermissionGranted();
+                }
+            }
+        } else {
+            if (mListener != null) {
+                mListener.onPermissionGranted();
+            }
+        }
+    }
+
+    /**
+     * 申请权限前判断是否需要声明
+     *
+     * @param desc
+     * @param permissions
+     * @param requestCode
+     */
+    private void requestEachPermissions(String desc, String[] permissions, int requestCode) {
+        if (shouldShowRequestPermissionRationale(permissions)) {// 需要再次声明
+            showRationaleDialog(desc, permissions, requestCode);
+        } else {
+            ActivityCompat.requestPermissions(PermissionActivity.this, permissions, requestCode);
+        }
+    }
+
+    /**
+     * 弹出声明的 Dialog
+     *
+     * @param desc
+     * @param permissions
+     * @param requestCode
+     */
+    private void showRationaleDialog(String desc, final String[] permissions, final int requestCode) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.tips))
+                .setMessage(desc)
+                .setPositiveButton(getResources().getString(R.string.confrim), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(PermissionActivity.this, permissions, requestCode);
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.cancle), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+
+    /**
+     * 再次申请权限时，是否需要声明
+     *
+     * @param permissions
+     * @return
+     */
+    private boolean shouldShowRequestPermissionRationale(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 检察每个权限是否申请
+     *
+     * @param permissions
+     * @return true 需要申请权限,false 已申请权限
+     */
+    private boolean checkEachSelfPermission(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 申请权限结果的回调
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == mRequestCode) {
+            if (checkEachPermissionsGranted(grantResults)) {
+                if (mListener != null) {
+                    mListener.onPermissionGranted();
+                }
+            } else {// 用户拒绝申请权限
+                if (mListener != null) {
+                    mListener.onPermissionDenied();
+                }
+            }
+        }
+    }
+
+    /**
+     * 检查回调结果
+     *
+     * @param grantResults
+     * @return
+     */
+    private boolean checkEachPermissionsGranted(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public interface PermissionsResultListener {
+
+        void onPermissionGranted();
+
+        void onPermissionDenied();
+    }
+}
